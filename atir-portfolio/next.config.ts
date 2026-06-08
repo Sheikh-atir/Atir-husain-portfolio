fc37b3cf74a7ed0fcee605faf097b725fa6c9051
unknown <sheikhhusainr47@gmail.com>
2026-06-08 13:31:25 +0530

    Fix Netlify deployment root directory

--- atir-portfolio/next.config.ts
+++ atir-portfolio/next.config.ts
@@ -1,9 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
   experimental: {},
   images: {
-    domains: [],
+    unoptimized: true,
   },
 };
 
